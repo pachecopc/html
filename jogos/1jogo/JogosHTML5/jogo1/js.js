f60e60206f36cf1f7117331b1c126d633864cf8a
Paulo <paulopacheco7@hotmail.com>
2023-05-14 02:07:54 -0300

    alterei

--- jogos/1jogo/JogosHTML5/jogo1/js.js
+++ jogos/1jogo/JogosHTML5/jogo1/js.js
@@ -10,6 +10,23 @@ function start() { // Inicio da função start()
 	//Principais variáveis do jogo
 	
 	var jogo = {}
+	var TECLA = {
+		W: 87,
+		S: 83,
+		D: 68
+		}
+	
+		jogo.pressionou = [];
+	//Verifica se o usuário pressionou alguma tecla	
+	
+	$(document).keydown(function(e){
+		jogo.pressionou[e.which] = true;
+		});
+	
+	
+		$(document).keyup(function(e){
+		   jogo.pressionou[e.which] = false;
+		});
 	
 	//Game Loop
 
@@ -18,11 +35,42 @@ function start() { // Inicio da função start()
 	function loop() {
 	
 	movefundo();
+	movejogador();
 	
 	} // Fim da função loop()
 
 	//Função que movimenta o fundo do jogo
 	
+	function movejogador() {
+	
+		if (jogo.pressionou[TECLA.W]) {
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo-10);
+
+			if (topo<=0) {
+		
+				$("#jogador").css("top",topo+10);
+			}
+			
+		
+		}
+		
+		if (jogo.pressionou[TECLA.S]) {
+			
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo+10);
+			if (topo>=434) {	
+				$("#jogador").css("top",topo-10);
+					
+			}	
+		}
+		
+		if (jogo.pressionou[TECLA.D]) {
+			
+			//Chama função Disparo	
+		}
+	
+		} // fim da função movejogador()
 	function movefundo() {
 	
 		esquerda = parseInt($("#fundoGame").css("background-position"));
